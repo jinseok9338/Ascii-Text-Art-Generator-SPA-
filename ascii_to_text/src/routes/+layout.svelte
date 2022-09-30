3dfa1753d5dc7d63873208f1c008ed04955bea5f
jinseok9338 <jinseok9338@gmail.com>
2022-09-30 02:49:31 +0000

    Make Svelte Kit app

---
+++ ascii_to_text/src/routes/+layout.svelte
@@ -0,0 +1,62 @@
+<script>
+	import Header from './Header.svelte';
+	import './styles.css';
+</script>
+
+<div class="app">
+	<Header />
+
+	<main>
+		<slot >
+		
+		</slot> 
+	</main>
+
+	<footer>
+		<p>Want to visit my <strong class="github-link" on:click={()=>{
+			window.open("https://www.github.com/jinseok9338")
+		}}>GitHub Page??</strong> </p>
+	</footer>
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	.github-link {
+		cursor: pointer;
+		color: red;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+</style>
---
+++ ascii_to_text/src/routes/Header.svelte
@@ -0,0 +1,74 @@
+<script>
+	import { page } from '$app/stores';
+	import logo from '$lib/images/svelte-logo.svg';
+	import github from '$lib/images/github.svg';
+</script>
+
+<header>
+	<div class="corner">
+		<a href="https://kit.svelte.dev">
+			<img src={logo} alt="SvelteKit" />
+		</a>
+	</div>
+
+	<nav>
+	
+	</nav>
+
+	<div class="corner">
+		<a href="https://github.com/sveltejs/kit">
+			<img src={github} alt="GitHub" />
+		</a>
+	</div>
+</header>
+
+<style>
+	header {
+		display: flex;
+	justify-content: space-between;
+
+    position: fixed;
+    width: 100vw;
+  
+    margin: 0 auto;
+    background-color: #fff;
+    font-family: var(--font);
+    z-index: 100;
+    user-select: none;
+    transition: transform .2s;
+	}
+
+	.corner {
+		width: 3em;
+		height: 3em;
+	}
+
+	.corner a {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 100%;
+	}
+
+	.corner img {
+		width: 2em;
+		height: 2em;
+		object-fit: contain;
+	}
+
+	nav {
+		display: flex;
+		justify-content: center;
+		--background: rgba(255, 255, 255, 0.7);
+	}
+
+
+
+
+
+
+	a:hover {
+		color: var(--color-theme-1);
+	}
+</style>
